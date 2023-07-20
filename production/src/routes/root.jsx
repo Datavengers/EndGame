f194cf1974f69f58befbc26473bf29b3b73d0514
abutela <butela.ashley@gmail.com>
2023-07-19 22:04:35 -0400

    add forgotten stashed changes

--- production/src/routes/root.jsx
+++ production/src/routes/root.jsx
@@ -49,91 +49,91 @@ export default function Root() {
         </div>
         <div id = "content-area">
             {/* add className="hidden" to sidebar div to hide */}
-            <div id="sidebar"> 
-            <h1>React Router Contacts</h1>
-            <div>
-                <Form id="search-form" role="search">
-                <input
-                    id="q"
-                    className={searching ? "loading" : ""}
-                    aria-label="Search contacts"
-                    placeholder="Search"
-                    type="search"
-                    name="q"
-                    defaultValue={q}
-                    // makes it so one click of back clears search
-                    onChange = {(event) => {
-                        const isFirstSearch = q == null;
-                        submit(event.currentTarget.form,{
-                            replace: !isFirstSearch,
-                        }); 
-                    }}
-                />
-                <div
-                    id="search-spinner"
-                    aria-hidden
-                    hidden={!searching}
-                />
-                <div
-                    className="sr-only"
-                    aria-live="polite"
-                ></div>
-                </Form>
-                <Form method="post">
-                    <button type="submit">New</button>
-                </Form>
-            </div>
-            <nav>
-            {contacts.length ? (
-                <ul>
-                {contacts.map((contact) => (
-                    <li key={contact.id}>
-                        <NavLink to={`contacts/${contact.id}`}
-                        className={({ isActive, isPending })=>
-                    //   make the nav links highlight if active
-                            isActive 
+            <div id="sidebar" className="hidden" style={{width:0}}> 
+                <h1>React Router Contacts</h1>
+                <div>
+                    <Form id="search-form" role="search">
+                    <input
+                        id="q"
+                        className={searching ? "loading" : ""}
+                        aria-label="Search contacts"
+                        placeholder="Search"
+                        type="search"
+                        name="q"
+                        defaultValue={q}
+                        // makes it so one click of back clears search
+                        onChange = {(event) => {
+                            const isFirstSearch = q == null;
+                            submit(event.currentTarget.form,{
+                                replace: !isFirstSearch,
+                            }); 
+                        }}
+                    />
+                    <div
+                        id="search-spinner"
+                        aria-hidden
+                        hidden={!searching}
+                    />
+                    <div
+                        className="sr-only"
+                        aria-live="polite"
+                    ></div>
+                    </Form>
+                    <Form method="post">
+                        <button type="submit">New</button>
+                    </Form>
+                </div>
+                <nav>
+                {contacts.length ? (
+                    <ul>
+                    {contacts.map((contact) => (
+                        <li key={contact.id}>
+                            <NavLink to={`contacts/${contact.id}`}
+                            className={({ isActive, isPending })=>
+                        //   make the nav links highlight if active
+                                isActive 
+                                    ? "active"
+                                    : isPending
+                                    ? "pending"
+                                    : ""
+                                }
+                            >
+                            {contact.first || contact.last ? (
+                            <>
+                                {contact.first} {contact.last}
+                            </>
+                            ) : (
+                            <i>No Name</i>
+                            )}{" "}
+                            {contact.favorite && <span>★</span>}
+                        </NavLink>
+                        </li>
+                    ))}
+                    </ul>
+                ) : (
+                    <p>
+                    <i>No contacts</i>
+                    </p>
+                )}
+                </nav>
+                <nav>
+                    <ul>
+                        <li>
+                            <NavLink 
+                            to={`game_map`}
+                            className={({ isActive, isPending })=>
+                            //   make the nav links highlight if active
+                                    isActive 
                                 ? "active"
                                 : isPending
                                 ? "pending"
                                 : ""
                             }
-                        >
-                        {contact.first || contact.last ? (
-                        <>
-                            {contact.first} {contact.last}
-                        </>
-                        ) : (
-                        <i>No Name</i>
-                        )}{" "}
-                        {contact.favorite && <span>★</span>}
-                    </NavLink>
-                    </li>
-                ))}
-                </ul>
-            ) : (
-                <p>
-                <i>No contacts</i>
-                </p>
-            )}
-            </nav>
-            <nav>
-                <ul>
-                    <li>
-                        <NavLink 
-                        to={`game_map`}
-                        className={({ isActive, isPending })=>
-                        //   make the nav links highlight if active
-                                isActive 
-                            ? "active"
-                            : isPending
-                            ? "pending"
-                            : ""
-                        }
-                        >Map
-                        </NavLink>
-                    </li>
-                </ul>
-            </nav>
+                            >Map
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
             </div>
             <div 
                 id="detail" 
