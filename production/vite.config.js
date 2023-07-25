2dc4a5eed6eb42cb16f226c506edecf42d2ac1dd
pghgal317 <108704479+pghgal317@users.noreply.github.com>
2023-07-25 07:50:05 -0400

    Updated backend_integration to main working version.

--- production/vite.config.js
+++ production/vite.config.js
@@ -2,13 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-<<<<<<< HEAD
-  plugins: [react()],
-  server: {
-    port: 3000, // Set the port for the development server
-  },
-});
-=======
   plugins: [react()]
 });
->>>>>>> 6e80c48057804df730c4fe4aa3fd5bfdfcd08680
