f1ba8b91d03246c493a4d88e6e6833990624135a
Jordan Rood <jrood2024@nevada.unr.edu>
2023-08-01 17:07:00 -0700

    SLL Learning Card added with page for content

--- production/src/main.jsx
+++ production/src/main.jsx
@@ -18,6 +18,7 @@ import Resources_Home from './routes/resource_home';
 import Signup from './routes/siginup';
 import TrainGUI from './lesson1/Train';
 import SinglyLinkedListQuiz from './routes/singlyLinkedListQuiz';
+import SinglyLinkedListLearning from './routes/sll_learning';
 
 const router = createBrowserRouter([
   {
@@ -44,6 +45,10 @@ const router = createBrowserRouter([
           {
             path:"singly-linked-lists/train-game",
             element:<TrainGUI />
+          },
+          {
+            path:"singly-linked-lists/sll-learning",
+            element:<SinglyLinkedListLearning />
           },
           {
             path: "doubly-linked-lists",
